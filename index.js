19b48664e2efe2d463aaa104206b732b903c3e84
Brian <brian.mwangi2@student.moringaschool.com>
2024-03-24 15:27:06 +0300

    adds files

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(activity='roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+/*
+function saturdayFun(activity=`bathe my dog`){
+    return `This Saturday, I want to ${activity}!`
+}
+*/
+//Mondays work
+const mondayWork= function(activity=`go to the office`){
+    return `This Monday, I will ${activity}.`
+}
+
+// Wrap ajdectivewrapAdjective=function ()
+const wrapAdjective =function(cost= `*`){
+    return function(object=`a hardworker`){
+        return `You are ${cost}${object}${cost}!`
+    }
+}
